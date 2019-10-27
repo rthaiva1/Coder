29806a633dbd8b224f5e2502aedb9201e9205d1f
Rosh Sugathan Thaivalappil <roshsugathanthaivalappil@Roshs-MacBook-Pro.local>
2019-10-27 13:31:01 -0400

    Displaying array successful

--- Leaderboard/Leaderboard/TableViewCell.swift
+++ Leaderboard/Leaderboard/TableViewCell.swift
@@ -1,28 +1,25 @@
 //
-//  ViewController.swift
+//  TableViewCell.swift
 //  Leaderboard
 //
-//  Created by Rosh Sugathan Thaivalappil on 10/24/19.
+//  Created by Rosh Sugathan Thaivalappil on 10/27/19.
 //  Copyright © 2019 Rosh Sugathan Thaivalappil. All rights reserved.
 //
 
 import UIKit
 
-class boardViewController: UIViewController
-{
-    
+class TableViewCell: UITableViewCell {
+
     @IBOutlet var team_o: UILabel!
     @IBOutlet var run_o: UILabel!
-    @IBOutlet var wickets_o: UILabel!
+    @IBOutlet var wicket_o: UILabel!
     @IBOutlet var overs_o: UILabel!
     
     func setcell(team: teamd)
     {
         team_o.text = team.name
         run_o.text = String(team.runs)
-        wickets_o.text = String(team.wickets)
+        wicket_o.text = String(team.wickets)
         overs_o.text = String(team.overs)
     }
 }
-
-
--- Leaderboard/Leaderboard/ViewController.swift
+++ Leaderboard/Leaderboard/ViewController.swift
@@ -10,58 +10,93 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+
+    @IBOutlet var tableteam: UITableView!
     @IBOutlet var team_label: UILabel!
     @IBOutlet var team_textbox: UITextField!
+    @IBOutlet var addbutton: UIButton!
     @IBOutlet var tlabel: UILabel!
     @IBOutlet var team: UITextField!
     @IBOutlet var slabel: UILabel!
     @IBOutlet var run: UITextField!
-    @IBOutlet var rlabel: UILabel!
     @IBOutlet var wicket: UITextField!
     @IBOutlet var wlabel: UILabel!
     @IBOutlet var olabel: UILabel!
     @IBOutlet var over: UITextField!
+    @IBOutlet var submit: UIButton!
     var teams: [teamd] = []
-    var list: [teamd] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-                team_label.isHidden = true
-                team_textbox.isHidden = true
-                tlabel.isHidden = true
-                team.isHidden = true
-                slabel.isHidden = true
-                run.isHidden = true
-                rlabel.isHidden = true
-                wicket.isHidden = true
-                wlabel.isHidden = true
-                olabel.isHidden = true
-                over.isHidden = true
-
+//                team_label.isHidden = true
+//                team_textbox.isHidden = true
+//                addbutton.isHidden = true
+//                tlabel.isHidden = true
+//                team.isHidden = true
+//                slabel.isHidden = true
+//                run.isHidden = true
+//                wicket.isHidden = true
+//                wlabel.isHidden = true
+//                olabel.isHidden = true
+//                over.isHidden = true
+//                submit.isHidden = true
+        teams = array()
     }
 
-    @IBAction func addteam(_ sender: UIButton) {
-        team_label.isHidden = false
-        team_textbox.isHidden = false
-        let temp = teamd(name: String(team_textbox.text!) ,runs: 0, wickets: 0, overs: 0.0)
-        list.append(temp)
-        teams = list
+    func array() -> [teamd]
+    {
+        var list: [teamd] = []
+        
+        let team1 = teamd(name: "abc" ,runs: 35, wickets: 2, overs: 46.0)
+        let team2 = teamd(name: "rosh" ,runs: 46, wickets: 3, overs: 34.0)
+        let team3 = teamd(name: "wow" ,runs: 65, wickets: 5, overs: 45.3)
+        let team4 = teamd(name: "titan" ,runs: 43, wickets: 6, overs: 2.0)
+        let team5 = teamd(name: "joey" ,runs: 43, wickets: 34, overs: 56.3)
+        let team6 = teamd(name: "mon" ,runs: 24, wickets: 5, overs: 56.2)
+        
+        list.append(team1)
+        list.append(team2)
+        list.append(team3)
+        list.append(team4)
+        list.append(team5)
+        list.append(team6)
+        
+        return list
+    
     }
     
+    @IBAction func addteam(_ sender: UIButton) {
+//        team_label.isHidden = false
+//        team_textbox.isHidden = false
+//        addbutton.isHidden = false
+    }
+    
+    @IBAction func add(_ sender: Any)
+    {
+//        let temp = teamd(name: String(team_textbox.text!) ,runs: 0, wickets: 0, overs: 0.0)
+//        teams.append(temp)
+//        teams = list
+//        team_label.isHidden = true
+//        team_textbox.isHidden = true
+//        addbutton.isHidden = true
+    }
     @IBAction func viewboard(_ sender: UIButton)
     {
+        teams = array()
+//        tableteam.beginUpdates()
         performSegue(withIdentifier: "viewtable", sender: self)
     }
     @IBAction func addstat(_ sender: UIButton) {
-        tlabel.isHidden = false
-        team.isHidden = false
-        slabel.isHidden = false
-        run.isHidden = false
-        rlabel.isHidden = false
-        wicket.isHidden = false
-        wlabel.isHidden = false
-        olabel.isHidden = false
-        over.isHidden = false
+//        tlabel.isHidden = false
+//        team.isHidden = false
+//        slabel.isHidden = false
+//        run.isHidden = false
+//        wicket.isHidden = false
+//        wlabel.isHidden = false
+//        olabel.isHidden = false
+//        over.isHidden = false
+//        submit.isHidden = false
         run.text = "0"
         wicket.text = "0"
         over.text = "0"
@@ -71,20 +106,32 @@ class ViewController: UIViewController {
         let r = Int(run.text!)
         let w = Int(wicket.text!)
         let o = Double(over.text!)
-          for teamd in teams
-          {
-            if teamd.name == team.text
-            {
-                teamd.runs = r! + teamd.runs
-                teamd.wickets = w! + teamd.wickets
-                teamd.overs = o! + teamd.overs
-            }
+        
+        if let row = self.teams.firstIndex(where: {$0.name == String(team.text!)})
+        {
+            teams[row].runs = r! + teams[row].runs
+            teams[row].wickets = w! + teams[row].wickets
+            teams[row].overs = o! + teams[row].overs
         }
+        
+//        tlabel.isHidden = true
+//        team.isHidden = true
+//        slabel.isHidden = true
+//        run.isHidden = true
+//        wicket.isHidden = true
+//        wlabel.isHidden = true
+//        olabel.isHidden = true
+//        over.isHidden = true
+//        submit.isHidden = true
     }
     
 //    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        let vc = segue.destination as! boardViewController
-//        vc.teams = self.teams
+////        let vc = segue.destination as! boardViewController
+////        vc.teams = self.teams
+////    if(tableteam != nil)
+////    {
+////    }
+//
 //    }
 }
 
@@ -96,9 +143,8 @@ extension ViewController :UITableViewDataSource, UITableViewDelegate
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let team = teams[indexPath.row]
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "teamcell") as! boardViewController
+        let cell = tableView.dequeueReusableCell(withIdentifier: "newcell") as! TableViewCell
         cell.setcell(team: team)
+        return cell
     }
-
 }
